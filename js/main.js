c9e109687eefbae8652dcecba051b9e9d3e3a544
Terence Kim <terence.taehee.kim@gmail.com>
2015-09-23 21:29:48 -0400

    V.0.0

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 
-// Establish all variables that your Analyser will use
+// Establishing all variables that the Analyser will use
 var audio, canvas, ctx, source, analyser, fbc_array, bars, bar_x, bar_width, bar_height, avg_bass, avg_mid, avg_high;
-var duration;
+var music, duration, pButton, playhead, timeline, timelineWidth;
 var sampleRate = 44100;
-// Initialize the MP3 player after the page loads all of its HTML into the window
-// window.addEventListener("load", initMp3Player, false);
-// document.getElementById("play").addEventListener("click", initAudio, false);
+// Boolean value so that mouse is moved on mouseUp only when the playhead is released 
+var onplayhead = false;
 
 $(document).ready(function (){
   adjustPositions();
@@ -28,7 +27,7 @@ function setTimelineWidth(){
 
 }
 
-$(".upload-button").on("change", doSomething);
+$(".upload-button").on("change", setAudio);
 
 
 $("#fileselect").on("change", initAudio);
@@ -37,8 +36,32 @@ function animateOverlay() {
   $(".upload-overlay").animate({ "top": "100%" }, "slow" );
 }
 
+function init(){
 
-function doSomething() {
+  music = audio;
+  duration = music.duration;  
+
+  pButton = document.getElementById('pButton'); // play button
+  playhead = document.getElementById('playhead'); // playhead
+  timeline = document.getElementById('timeline'); // timeline
+  // timeline width adjusted for playhead
+  timelineWidth = timeline.offsetWidth - playhead.offsetWidth;
+
+  // timeupdate event listener
+  music.addEventListener("timeupdate", timeUpdate, false);
+
+  //Makes timeline clickable
+  timeline.addEventListener("click", function (event) {
+    moveplayhead(event);
+    music.currentTime = duration * clickPercent(event);
+  }, false);
+
+  // Makes playhead draggable 
+  playhead.addEventListener('mousedown', mouseDown, false);
+  window.addEventListener('mouseup', mouseUp, false);
+}
+
+function setAudio() {
 
   animateOverlay();
   audio = new Audio();
@@ -50,10 +73,7 @@ function doSomething() {
 }
 
 function initAudio(){
-  console.log("yo")
   audio = {};
-
-
   audio = new Audio();
   audio.controls = false;
   audio.loop = true;
@@ -69,7 +89,6 @@ function initMp3Player(audio){
 
 
   audio.addEventListener("loadeddata", function() {
-    console.log("data laoded")
     duration = this.duration;
     init();
     timeUpdate();
@@ -78,11 +97,6 @@ function initMp3Player(audio){
 
 
   });
-
-
-
-  // pButton.className = "";
-  // pButton.className = "play";
 
   analyser = context.createAnalyser(); // AnalyserNode method
 
@@ -93,18 +107,16 @@ function initMp3Player(audio){
   analyser.fftSize = 1024;
   frameLooper();
 }
+
 // frameLooper() animates any style of graphics you wish to the audio frequency
 // Looping at the default frame rate that the browser provides(approx. 60 FPS)
 function frameLooper(){
   window.requestAnimationFrame(frameLooper);
-  //instantiating array of with same size as analyser frequency bin count
 
-  //analyser.frequencyBinCount = 0 - 256
   fbc_array = new Uint8Array(analyser.frequencyBinCount);
   frequencies = new Float32Array(analyser.frequencyBinCount);
   analyser.getFloatFrequencyData(frequencies);
-  // frequency = calculateHertz(frequencies);
-  //values here are from 0-255
+
   //converting the analyser frequency into javascript array
   analyser.getByteFrequencyData(fbc_array);
 
@@ -119,9 +131,6 @@ function frameLooper(){
   animateChest(fbc_array);
   animateArms(fbc_array);
   animateLegs(fbc_array);
-
-  // animateEyes(frequency)
-  // console.log(fbc_array[0]);
 }
 
 function animateHead(fbc){
@@ -139,7 +148,6 @@ function animateBody(fbc){
   var bodyBassScale = (parseFloat(avg_bass)/230); //180 seems reasonably normalish
 
   if(bodyBassScale >= 1) {
-    // $(".upperbody").css("transform", "scale(" + bodyBassScale + ")");
     $(".lowerbody").css("transform", "scale(" + bodyBassScale + ")");
   }
 }
@@ -159,10 +167,9 @@ function animateMouth(fbc){
   for(var i = 10; i < 27; i++) {
     tot_mid = tot_mid + fbc[i];
   }
-  // console.log(tot_mid)
   var avg_mid = tot_mid/17;
 
-  var midScale = (parseFloat(avg_mid)/180); //180 seems reasonably normalish
+  var midScale = (parseFloat(avg_mid)/180);
   if (midScale >= 0.7) {
     $(".inside-mouth").css("height", (0 + Math.pow(1+midScale, 5)));
   }
@@ -178,13 +185,10 @@ function animateSnapBack(fbc) {
     tot_high = tot_high + fbc[i];
   }
 
-   // console.log(tot_mid)
   var avg_high = tot_high/100;
 
-  // console.log(avg_high);
-  var highScale = (parseFloat(avg_high)/180); //180 seems reasonably normalish
+  var highScale = (parseFloat(avg_high)/180);
   if (highScale >= 0.6) {
-    // var weird = highScale - 0.7
     $(".snapback-body").css("background-color", "hsl(" +(((highScale-0.6)*(340-207)/0.6) + 207) + ",75%, 60%)");
   }
   if (highScale == 0) {
@@ -198,23 +202,17 @@ function animatePupils(fbc) {
     tot_high = tot_high + fbc[i];
   }
 
-   // console.log(tot_mid)
   var avg_high = tot_high/100;
 
-  // console.log(avg_high);
-  var highScale = (parseFloat(avg_high)/180); //180 seems reasonably normalish
+  var highScale = (parseFloat(avg_high)/180);
   if (highScale >= 0.6) {
-    // var weird = highScale - 0.7
     $(".pupil").css("background-color", "hsl(100,"+ (((highScale-0.6)*(80)/0.6))+ "%, 60%)");
   }
-  // if (highScale == 0) {
-  //   $(".inside-mouth").animate({ "background-color": "steelblue" }, "fast" )
-  // }
 }
 
 function animateChest(fbc) {
   var avg_bass = (fbc[0] + fbc[1])/2; 
-  var chestBassScale = (parseFloat(avg_bass)/220); //180 seems reasonably normalish
+  var chestBassScale = (parseFloat(avg_bass)/220);
 
   if(chestBassScale >= 1) {
     $(".chest").css("transform", "scale(" + (chestBassScale) + ")");
@@ -225,13 +223,10 @@ function animateChest(fbc) {
     tot_high = tot_high + fbc[i];
   }
 
-   // console.log(tot_mid)
   var avg_high = tot_high/100;
 
-  // console.log(avg_high);
-  var highScale = (parseFloat(avg_high)/180); //180 seems reasonably normalish
+  var highScale = (parseFloat(avg_high)/180);
   if (highScale >= 0.6) {
-    // var weird = highScale - 0.7
     $(".chest").css("background-color", "hsl(" +(((highScale-0.6)*(0-182)/0.6) + 182) + "," + (((highScale-0.6)*(100-25)/0.6) + 25) + "%, 50%)");
     $(".chest").css("border-color", "hsl(" +(((highScale-0.6)*(0-182)/0.6) + 182) + "," + (((highScale-0.6)*(100-25)/0.6) + 25) + "%, 50%)");
   }
@@ -240,7 +235,7 @@ function animateChest(fbc) {
 
 function animateArms(fbc) {
   var avg_bass = (fbc[0] + fbc[1])/2; 
-  var armBassScale = (parseFloat(avg_bass)/240); //180 seems reasonably normalish
+  var armBassScale = (parseFloat(avg_bass)/240);
   var rotatingDegree = 8+Math.pow(armBassScale, 40);
   if(armBassScale >= 1) {
     $(".left-arm").css("transform", "rotateZ(" + rotatingDegree+"deg) scale(" + (armBassScale) + ")");
@@ -251,13 +246,10 @@ function animateArms(fbc) {
     tot_high = tot_high + fbc[i];
   }
 
-   // console.log(tot_mid)
   var avg_high = tot_high/100;
 
-  // console.log(avg_high);
-  var highScale = (parseFloat(avg_high)/150); //180 seems reasonably normalish
+  var highScale = (parseFloat(avg_high)/150);
   if (highScale >= 0.6) {
-    // var weird = highScale - 0.7
     $(".arm").css("background-color", "hsl(" +(((highScale-0.6)*(0-100)/0.6)) + "," + (((highScale-0.6)*(100-68)/0.6) + 68) + "%, 42%)");
   }
 
@@ -269,17 +261,12 @@ function animateLegs(fbc) {
     tot_high = tot_high + fbc[i];
   }
 
-   // console.log(tot_mid)
   var avg_high = tot_high/100;
 
-  // console.log(avg_high);
-  var highScale = (parseFloat(avg_high)/130); //180 seems reasonably normalish
+  var highScale = (parseFloat(avg_high)/130);
   if (highScale >= 0.6) {
-    // $(".legs").css("background-color", "hsl(180,25%," + (((highScale-0.6)*(0-25)/0.6) + 25) + "%)");
     $(".legs").css("background-color", "hsl(" +(((highScale-0.6)*(360-180)/0.6) + 180) + "," + (((highScale-0.6)*(100-25)/0.6) + 25) + "%, 25%)");
-
   }
-
 }
 
 function changeBackground(fbc) {
@@ -288,61 +275,14 @@ function changeBackground(fbc) {
     tot_high = tot_high + fbc[i];
   }
 
-   // console.log(tot_mid)
   var avg_high = tot_high/200;
 
-  // console.log(avg_high);
-  var highScale = (parseFloat(avg_high)/180); //180 seems reasonably normalish
+  var highScale = (parseFloat(avg_high)/180); 
   if (highScale >= 0.6) {
-    // $(".legs").css("background-color", "hsl(180,25%," + (((highScale-0.6)*(0-25)/0.6) + 25) + "%)");
     $("body").css("background-color", "hsl(" +(((highScale-0.6)*(0-340)/0.6) + 340) + ",60%, 65%)");
     $(".thigh-gap").css("background-color", "hsl(" +(((highScale-0.6)*(0-340)/0.6) + 340) + ",60%, 65%)");
 
   }
-}
-
-var music, duration, pButton, playhead, timeline, timelineWidth;
-  // Boolean value so that mouse is moved on mouseUp only when the playhead is released 
-  var onplayhead = false;
-
-
-function init(){
-
-
-  // music = document.getElementById('controls').getElementsByTagName('audio')[0]; // play button
-  music = audio;
-  // console.log(audio);
-  // console.log(document.getElementById('controls').getElementsByTagName('audio')[0]);
-  duration = music.duration;  
-
-  // console.log(duration);
-  pButton = document.getElementById('pButton'); // play button
-
-  playhead = document.getElementById('playhead'); // playhead
-
-  timeline = document.getElementById('timeline'); // timeline
-  // timeline width adjusted for playhead
-  timelineWidth = timeline.offsetWidth - playhead.offsetWidth;
-
-
-
-  // timeupdate event listener
-  music.addEventListener("timeupdate", timeUpdate, false);
-
-  //Makes timeline clickable
-  timeline.addEventListener("click", function (event) {
-    moveplayhead(event);
-    console.log(duration)
-    music.currentTime = duration * clickPercent(event);
-  }, false);
-
-  // Makes playhead draggable 
-  playhead.addEventListener('mousedown', mouseDown, false);
-  window.addEventListener('mouseup', mouseUp, false);
-
-
-
-  // play(music);
 }
 
 // returns click as decimal (.77) of the total timelineWidth
@@ -383,13 +323,9 @@ function moveplayhead(e) {
   }
 }
 
-// timeUpdate 
 // Synchronizes playhead position with current point in audio 
 function timeUpdate() {
-  console.log("word")
   var playPercent = timelineWidth * (music.currentTime / music.duration);
-  console.log(music.duration)
-  console.log(music.currentTime)
   playhead.style.marginLeft = playPercent + "px";
   if (music.currentTime == duration) {
     pButton.className = "";
@@ -399,7 +335,6 @@ function timeUpdate() {
 
 //Play and Pause
 function play() {
-  //concole.
   // start music
   if (music.paused) {
     music.play();
